5b94a06c55aab43999b5a2f3031673eb8976f92c
Pavel Fedin <pavel_fedin@mail.ru>
2022-07-31 14:12:59 +0300

    Rewritten sequencer jump decodning
    Complex combinartions of AR, STK0 and constants may be used together with
    OR-based switches. Rewrite the code to handle them all in a unified way

--- Microcode.py
+++ Microcode.py
@@ -6,7 +6,7 @@ ALU_OP_MAP = ['{r}+{s}', '{s}-{r}', '{r}-{s}', '{r}|{s}', '{r}&{s}', '(~{r})&{s}
 ALU_MEM_DEST_MAP = ['', '', 'r{b}={f}', 'r{b}={f}', 'r{b}=({f})>>1', 'r{b}=({f})>>1', 'r{b}=({f})<<1', 'r{b} =({f})<<1']
 ALU_Q_DEST_MAP = ['Q={f}', ''     , ''     , ''     , 'Q>>=1', ''     , 'Q<<=1', '']
 ALU_OUT_MAP    = ['Y={f}', 'Y={f}', 'Y={a}', 'Y={f}', 'Y={f}', 'Y={f}', 'Y={f}', 'Y={f}']
-RESULT_MAP = ['', 'Result', 'RIndex', 'Level', 'PTIndex', 'MemAddr_Swap', 'Seq', 'CC']
+RESULT_MAP = ['', 'Result', 'RIndex', 'Level', 'PTIndex', 'Swap', 'AR', 'CC']
 
 # Microcode ROMs order is denoted by a letter A-F.
 # They are arranged from MSB to LSB: ABCDEFH
@@ -29,7 +29,7 @@ class MicroCode(object):
         return (word >> start) & (~(-1 << size))
 
     def disassemble(self):
-        print('Addr D          ALU Op                           F         Result Sel   Seq Op')
+        print('Addr DP         ALU Op                           F         Result Sel   Seq Op')
         for addr, word in enumerate(self.code):
             self.disassembleOne(addr, word)
         print()
@@ -42,69 +42,102 @@ class MicroCode(object):
             print(f'{addr:3x} {word:13x} unused')
             return
         seq0_din = self.getBits(word, 16, 4)
-        seq0_s = ~self.getBits(word, 29, 2) & 3
         seq1_din = self.getBits(word, 20, 4)
-        seq1_s = ~self.getBits(word, 29, 2) & 3 # pipeline 54
         seq2_din = self.getBits(word, 24, 3)
-        s0 = ~self.getBits(word, 31, 1) & 1
-        p32 = self.getBits(word, 32, 1)
-        p54 = self.getBits(word, 54, 1)
         # S1, S0 lines of sequencers are derived using some extra logic
-        # (U_J6, U_K6, U_L6)
-        s1 = ~(~(p54 & (~p32 & 1) & 1)) & 1
-        seq2_s = s1 << 1 | s0
+        seq0_s = self.getBits(word, 29, 2) ^ 3 # U_L6A, U_L6D        
+        sh0    = self.getBits(word, 31, 1) ^ 1 # U_K6A
+        mw_c40 = self.getBits(word, 32, 1)
+        s21    = mw_c40 ^ 1                    # U_K6B
+        mw_a6  = self.getBits(word, 54, 1)
+        s11    = mw_a6 & (mw_c40 ^ 1) # U_J6C, U_K6D, U_K6C
+        # Switch statement and condition selector
         case_ = self.getBits(word, 33, 1)
-        j13 = self.getBits(word, 20, 2)
-        k13 = self.getBits(word, 22, 2)
+        cond = self.getBits(word, 20, 4)
+        # Din for sequencers
         dest = (seq2_din << 8) | (seq1_din << 4) | seq0_din
-        s1s0 = (seq2_s << 8) | (seq1_s << 4) | seq0_s
+        # S inputs for sequencers
+        s1s0 = (s21 << 9) | (sh0 << 8) | (s11 << 5) | (sh0 << 4) | seq0_s
+        # Stack control for sequencers
+        fe  = self.getBits(word, 27, 1)
+        pup = self.getBits(word, 28, 1)
+
         self.selects[s1s0] += 1
         next = addr + 1
-        if case_ == 0:
-            # flags   zHCVMZ
-            if s1s0 == 0x033:
-                dest = (next & 0xf00) | (dest & 0x0ff)
-            elif s1s0 == 0x233:
-                pass # TODO: high nibble is AR (happens once)
-            elif s1s0 == 0x233:
-                pass # TODO: high nibble is stack (rare case)
-            jump = f'jump {dest:3x}'
-            if j13 == 0:
-                jump = f'switch flags(ZM) jump {dest|0:x}, {dest|1:x}, {dest|2:x}, {dest|3:x}'
-            elif j13 == 1:
-                jump = f'switch flags(VH) jump {dest|0:x}, {dest|1:x}, {dest|2:x}, {dest|3:x}'
+
+        if fe == 0 and pup == 1:
+            push = f'push {next:x}; '
         else:
-            fe = self.getBits(word, 27, 1)
-            pup = self.getBits(word, 28, 1)
-            jump = ''
-            if fe == 0:
-                if pup == 1:
-                    jump = f'push {next:x} '
-                elif s1s0 == 0x222:
-                    jump = f'ret '
-            if s1s0 == 0x333:
-                jump = f'{jump}jump {dest:3x}'
-            elif s1s0 == 0x111:
-                jump = f'{jump}jump AR'
-            elif s1s0 == 0x033:
-                dest = (next & 0xf00) | (dest & 0x0ff)
-                jump = f'{jump}jump {dest:3x}'
-            elif s1s0 == 0x133:
-                dest = dest & 0x0ff
-                jump = f'{jump}jump AR|{dest:3x}'
-            elif s1s0 == 0x311:
-                dest = dest & 0xf00
-                jump = f'{jump}jump {dest:3x}|AR|AR'
-            elif s1s0 == 0x300:
-                dest = (dest & 0xf00) | (next & 0x0ff)
-                jump = f'{jump}jump {dest:3x}'
-            elif s1s0 != 0 and s1s0 != 0x222:
-                jump = f'{jump}jump somewhere mux: {s1s0:3x}'
+            push = ''
+
+        # Some shorthands
+        # jump AR isn't really supported because AR isn't used on the third sequencer;
+        # it's AE line is permanently tied high.
+        if s1s0 == 0x000 and case_ == 1:
+            jump = '' # jump uPC (next)
+        elif s1s0 == 0x222 and case_ == 1:
+            if fe == 0 and pup == 0 :            
+                jump = 'ret' # jump STK0; pop
+            else:
+                jump = 'jump STK0'
+        else:
+            ar_mask    = 0;
+            pop_mask   = 0;
+            const_mask = 0;
+            target     = 0;
+
+            for i in range(0, 3):
+                bits = (s1s0 >> (i * 4)) & 3
+                mask = 0xf << (i * 4)
+                if bits == 0:
+                    target |= next & mask
+                    const_mask |= mask
+                elif bits == 1:
+                    ar_mask |= mask
+                elif bits == 2:
+                    pop_mask |= mask
+                else: # bits == 3
+                    target |= dest & mask
+                    const_mask |= mask
+
+            if case_ == 1:
+                jump = f'jump {target:x}'
+            elif const_mask & 0x00f == 0:
+                # Since the switch is OR-based, we need the less significant 4 bits
+                # to have a known base value, so we always take them from a constant
+                jump += ' !WARN unexpected switch'
+            # The first 4 branches use OR lines 0 and 1 via U_J13
+            elif cond == 0b1100:
+                jump = f'switch flags(ZM) jump ({target|0:x}, {target|1:x}, {target|2:x}, {target|3:x})'
+            elif cond == 0b1101:
+                jump = f'switch flags(VH) jump ({target|0:x}, {target|1:x}, {target|2:x}, {target|3:x})'
+            elif cond == 0b1110:
+                jump = f'switch pagetable??? jump ({target|0:x}, {target|1:x}, {target|2:x}, {target|3:x})'
+            # These branches use OR lines 2 and 3 via U_K13
+            elif cond == 0b0011:
+                jump = f'switch flags(IL) jump ({target|0:x}, {target|4:x}, {target|8:x}, {target|12:x})'
+            elif cond == 0b0111:
+                jump = f'switch interrupts??? jump ({target|0:x}, {target|4:x}, {target|8:x}, {target|12:x})'
+            elif cond == 0b1011:
+                jump = f'switch dma??? jump ({target|0:x}, {target|4:x}, {target|8:x}, {target|12:x})'
+            # Combined switches don't make sense
+            else:
+                jump += f'!WARN bad switch {cond:x}'
+
+            if pop_mask != 0:
+                jump += f'|(STK0 & {pop_mask:x})'
+            if ar_mask != 0:
+                jump += f'|(AR & {ar_mask:x})'
+
+            if fe == 0 and pup == 0:
+                jump += '; pop'
+
+        seq_op = push + jump
         dpBus = self.getDPBus(word)
         aluOp = self.getALUCode(word)
         fBus = self.getFBus(word)
         result = self.getResult(word)
-        print(f'{addr:3x}: {dpBus:10s} {aluOp:32s} {fBus:9s} {result:12s} {jump}')
+        print(f'{addr:3x}: {dpBus:10s} {aluOp:32s} {fBus:9s} {result:12s} {seq_op}')
 
     def getDPBus(self, word):
         d2d3 = self.getBits(word, 0, 4)
